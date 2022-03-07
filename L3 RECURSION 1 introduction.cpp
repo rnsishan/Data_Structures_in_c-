e9079cf861c17493b64be3fcc31e9a2743151a7f
rnsishan <ishankoul02@gmail.com>
2022-03-07 19:27:17 +0530

    Create L3 RECURSION 1 introduction.cpp

---
+++ L3 RECURSION 1 introduction.cpp
@@ -0,0 +1,24 @@
+//L3 RECURSION 1
+//INTRODUCTION TO RECURSION
+#include <iostream>
+
+using namespace std;
+
+int factorial(int n){
+    if(n==0){
+    
+        return 1;
+    }
+    int smallOutput=factorial(n-1);
+    return n*smallOutput;
+}
+
+int main()
+{
+    int n;
+    cin>>n;
+    int output=factorial(n);
+    cout<<output<<endl;
+
+    return 0;
+}
