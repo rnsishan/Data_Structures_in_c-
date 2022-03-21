1309d661a65f05c73619f54684833456284e9f24
rnsishan <ishankoul02@gmail.com>
2022-03-21 18:43:25 +0530

    Create recursion 1b tower of hanoi.cpp

---
+++ recursion 1b tower of hanoi.cpp
@@ -0,0 +1,27 @@
+/*
+Tower of Hanoi
+Send Feedback
+Tower of Hanoi is a mathematical puzzle where we have three rods and n disks. The objective of the puzzle is to move all disks from source rod to destination rod using third rod (say auxiliary). The rules are :
+1) Only one disk can be moved at a time.
+2) A disk can be moved only if it is on the top of a rod.
+3) No disk can be placed on the top of a smaller disk.
+Print the steps required to move n disks from source rod to destination rod.
+Source Rod is named as 'a', auxiliary rod as 'b' and destination rod as 'c'.
+*/
+#include <iostream>
+using namespace std;
+
+void towerOfHanoi(int n, char S, char T, char D) {
+    // Write your code here
+	if(n==0)
+        return;
+    towerOfHanoi(n-1,S,D,T);
+    cout<<S<<" "<<D<<endl;
+    towerOfHanoi(n-1,T,S,D);
+    
+}
+int main() {
+    int n;
+    cin >> n;
+    towerOfHanoi(n, 'a', 'b', 'c');
+}
