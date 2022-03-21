2c6fd9083d8478cc84bec687c6adbc459f71a242
rnsishan <ishankoul02@gmail.com>
2022-03-21 18:17:02 +0530

    Create L3 Sum of Array using recursion.cpp

---
+++ L3 Sum of Array using recursion.cpp
@@ -0,0 +1,36 @@
+//L3 Sum of Array using recursion
+/*Given an array of length N, you need to find and return the sum of all elements of the array.*/
+
+#include<iostream>
+using namespace std;
+
+int sum(int input[], int n) {
+  
+    if(n==0){
+        return 0;
+    }
+    if(n==1){
+        return input[0];
+    }
+    int sumt=0; 
+    sumt= input[0]+sum(input+1,n-1);
+	
+    return sumt;
+
+}
+
+int main(){
+    int n;
+    cin >> n;
+  
+    int *input = new int[n];
+    
+    for(int i = 0; i < n; i++) {
+        cin >> input[i];
+    }
+    
+    cout << sum(input, n) << endl;
+}
+
+
+
