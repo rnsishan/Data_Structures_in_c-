906f52814ffb348fab161c90e0cad4132b7b16c4
rnsishan <ishankoul02@gmail.com>
2022-04-04 22:18:13 +0530

    Create intro to OOPS.cpp

---
+++ OOPS 1/intro to OOPS.cpp
@@ -0,0 +1,35 @@
+/*
+L6 OOPS 1 
+introduction to OOPS
+
+objects possess some property and fxns
+
+like student has name,rollno,age,address etc.
+fxn like changeaddress,setrollno etc.
+
+1.class defines properties and fxns of a particular object
+2.object hua specific student of a class,class me saari properties hongi uski
+
+*/
+
+#include <iostream>
+
+using namespace std;
+/*
+
+SYNTAX FOR CLASS
+
+class student{
+  int rollNumber;
+  int age;
+};
+
+*/
+
+
+int main()
+{
+    
+
+    return 0;
+}
